8ea5b7336f253f34457c8d174fe57e5774cf745a
Shanthoosh Venkataraman <santhoshvenkat1988@gmail.com>
2018-06-01 11:10:35 -0700

    Failure testing initial commit.

---
+++ samza-test/src/main/python/tests/standalone_tests.py
@@ -0,0 +1,76 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+import util
+import logging
+import zopkio.runtime as runtime
+from kafka import SimpleProducer, SimpleConsumer
+
+logger = logging.getLogger(__name__)
+
+JOB_ID = 'test-app-id'
+PACKAGE_ID = 'tests'
+TEST_INPUT_TOPIC = 'testKafkaInputTopic'
+TEST_OUTPUT_TOPIC = 'testKafkaOutputTopic'
+NUM_MESSAGES = 50
+
+def test_samza_job():
+    """
+    Runs a job that reads converts input strings to integers, negates the
+    integer, and outputs to a Kafka topic.
+    """
+    _load_data()
+
+    deployer_name_to_config = {
+        'standalone-processor-1' : 'config/standalone.failure.test-processor-1.properties',
+        'standalone-processor-2' : 'config/standalone.failure.test-processor-2.properties',
+        'standalone-processor-3' : 'config/standalone.failure.test-processor-3.properties'
+    }
+
+    for deployer in ['standalone-processor-1', 'standalone-processor-2', 'standalone-processor-3']:
+        config_file = deployer_name_to_config[deployer]
+        util.start_job(PACKAGE_ID, JOB_ID, config_file, deployer)
+        util.await_job(PACKAGE_ID, JOB_ID, deployer)
+
+def validate_samza_job():
+    """
+    Validates that negate-number negated all messages, and sent the output to
+    samza-test-topic-output.
+    """
+    logger.info('Running validate_samza_job')
+    kafka = util.get_kafka_client()
+    kafka.ensure_topic_exists(TEST_OUTPUT_TOPIC)
+    consumer = SimpleConsumer(kafka, 'samza-test-group', TEST_OUTPUT_TOPIC)
+    messages = consumer.get_messages(count=NUM_MESSAGES, block=True, timeout=300)
+    message_count = len(messages)
+    kafka.close()
+
+def _load_data():
+    """
+    Sends 50 messages (1 .. 50) to samza-test-topic.
+    """
+    logger.info('Running test_samza_job')
+    kafka = util.get_kafka_client()
+    kafka.ensure_topic_exists(TEST_INPUT_TOPIC)
+    producer = SimpleProducer(
+        kafka,
+        async=False,
+        req_acks=SimpleProducer.ACK_AFTER_CLUSTER_COMMIT,
+        ack_timeout=30000)
+    for i in range(1, NUM_MESSAGES + 1):
+        producer.send_messages(TEST_INPUT_TOPIC, str(i))
+    kafka.close()
