579ae232b11245262238d85f0040b6e9f6732f1f
Shanthoosh Venkataraman <santhoshvenkat1988@gmail.com>
2018-06-05 15:19:10 -0700

    Scaffold commit.

--- samza-test/src/main/python/standalone_deployment.py
+++ samza-test/src/main/python/standalone_deployment.py
@@ -74,7 +74,7 @@ def _create_kafka_topic(zookeeper_servers, topic_name, partition_count, replicat
     base_dir = os.getcwd()
     logger.info('Current working directory: {0}'.format(base_dir))
 
-    create_topic_command = 'sh {0}/deploy/kafka/kafka_2.10-0.10.1.1/bin/kafka-topics.sh --create --zookeeper {1} --replication-factor {2} --partitions {3} --topic {4}'.format(base_dir, zookeeper_servers, replication_factor, partition_count, topic_name)
+    create_topic_command = ['sh {0}/deploy/kafka/kafka_2.10-0.10.1.1/bin/kafka-topics.sh --create --zookeeper {1} --replication-factor {2} --partitions {3} --topic {4}'.format(base_dir, zookeeper_servers, replication_factor, partition_count, topic_name)]
     logger.info("running command")
     logger.info(create_topic_command)
     p = Popen(create_topic_command, stdin=PIPE, stdout=PIPE, stderr=PIPE)
