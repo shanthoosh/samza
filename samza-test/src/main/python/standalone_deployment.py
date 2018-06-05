1bb8e8d9d701ab7ee69db46ed64db441701d30d7
Shanthoosh Venkataraman <santhoshvenkat1988@gmail.com>
2018-06-05 14:44:01 -0700

    Scaffold commit.

--- samza-test/src/main/python/standalone_deployment.py
+++ samza-test/src/main/python/standalone_deployment.py
@@ -17,21 +17,20 @@
 
 import os
 import logging
-import shutil
 import urllib
+import time
 import zopkio.runtime as runtime
 import zopkio.adhoc_deployer as adhoc_deployer
 from zopkio.runtime import get_active_config as c
 from subprocess import PIPE, Popen
 
-import time
-
 logger = logging.getLogger(__name__)
-deployers = None
+deployers = {}
 
+### Download all the components.
 def _download_components():
     for url_key in ['url_kafka', 'url_zookeeper']:
-        logger.debug('Getting download URL for: {0}'.format(url_key))
+        logger.info('Getting download URL for: {0}'.format(url_key))
         url = c(url_key)
         filename = os.path.basename(url)
         if os.path.exists(filename):
@@ -40,65 +39,59 @@ def _download_components():
             logger.info('Downloading: {0}'.format(url))
             urllib.urlretrieve(url, filename)
 
-def _new_ssh_deployer(config_prefix, name=None):
-    deployer_name = config_prefix if name == None else name
-    return adhoc_deployer.SSHDeployer(deployer_name, {
-        'install_path': os.path.join(c('remote_install_path'), c(config_prefix + '_install_path')),
-        'executable': c(config_prefix + '_executable'),
-        'post_install_cmds': c(config_prefix + '_post_install_cmds', []),
-        'start_command': c(config_prefix + '_start_cmd'),
-        'stop_command': c(config_prefix + '_stop_cmd'),
-        'extract': True,
-        'sync': True,
-    })
+###
+def _deploy_components(components):
+    global deployers
 
-def _deploy_components(deployers, components):
     for component in components:
-        deployer = deployers[component]
+        deployer = adhoc_deployer.SSHDeployer(component, {
+            'install_path': os.path.join(c('remote_install_path'), c(component + '_install_path')),
+            'executable': c(component + '_executable'),
+            'post_install_cmds': c(component + '_post_install_cmds', []),
+            'start_command': c(component + '_start_cmd'),
+            'stop_command': c(component + '_stop_cmd'),
+            'extract': True,
+            'sync': True,
+        })
+        deployers[component] = deployer
         runtime.set_deployer(component, deployer)
         for instance, host in c(component + '_hosts').iteritems():
             logger.info('Deploying {0} on host: {1}'.format(instance, host))
             deployer.deploy(instance, {
-                 'hostname': host
+                'hostname': host
             })
 
             time.sleep(5)
 
-## zookeeper_servers: Comma seperated list of zookeeper server of form host:port.
-## topic_name: Name of kafka topic to create.
-## partition_count: Number of partitions of kafka topic to be created.
-## replication_factor: Replication factor property of the kafka topic.
+## zookeeper_servers: Comma seperated list of zookeeper servers of the form host:port.
+## topic_name: kafka topic to be created.
+## partition_count: Number of partitions of the kafka topic.
+## replication_factor: Replication factor of the kafka topic.
 def _create_kafka_topic(zookeeper_servers, topic_name, partition_count, replication_factor):
-    ### Employ command line to create kafka topic since existing kafka python API doesn't allow to configure partition count when creating topic.
+
+    ### Using command line utility to create kafka topic since kafka python API doesn't support configuring partitionCount during topic creation.
+
     base_dir = os.getcwd()
     logger.info('Current working directory: {0}'.format(base_dir))
 
-    command='sh {0}/deploy/kafka/kafka_2.10-0.10.1.1/bin/kafka-topics.sh --create --zookeeper {1} --replication-factor {2} --partitions {3} --topic {4}'.format(base_dir, zookeeper_servers, replication_factor, partition_count, topic_name)
+    create_topic_command = 'sh {0}/deploy/kafka/kafka_2.10-0.10.1.1/bin/kafka-topics.sh --create --zookeeper {1} --replication-factor {2} --partitions {3} --topic {4}'.format(base_dir, zookeeper_servers, replication_factor, partition_count, topic_name)
     logger.info("running command")
-    logger.info(command)
-    p = Popen(command, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+    logger.info(create_topic_command)
+    p = Popen(create_topic_command, stdin=PIPE, stdout=PIPE, stderr=PIPE)
     output, err = p.communicate()
     logger.info("Output from kafka-topics.sh:\nstdout: {0}\nstderr: {1}".format(output, err))
 
+### Zopkio specific method that will be run before all the integration test once.
 def setup_suite():
-    global deployers
-
-    deployers = {
-        'zookeeper': _new_ssh_deployer('zookeeper'),
-        'kafka': _new_ssh_deployer('kafka'),
-        'standalone-processor-1': _new_ssh_deployer('standalone-processor-1'),
-        'standalone-processor-2': _new_ssh_deployer('standalone-processor-2'),
-        'standalone-processor-3': _new_ssh_deployer('standalone-processor-3'),
-    }
-
     _download_components()
 
-    _deploy_components(deployers, ['zookeeper', 'kafka'])
+    _deploy_components(['zookeeper', 'kafka'])
 
     _create_kafka_topic('localhost:2181','standaloneIntegrationTestKafkaInputTopic', 3, 1)
 
-    _deploy_components(deployers, ['standalone-processor-1', 'standalone-processor-2', 'standalone-processor-3'])
+    _deploy_components(['standalone-processor-1', 'standalone-processor-2', 'standalone-processor-3'])
 
+### Zopkio specific method that will be run after all the integration test once.
 def teardown_suite():
     # stream_application_deployer.uninstall('tests')
 
