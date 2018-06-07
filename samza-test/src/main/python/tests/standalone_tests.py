42aa491a8843a7093e70b4635f8431b52a9c4881
Shanthoosh Venkataraman <santhoshvenkat1988@gmail.com>
2018-06-07 16:27:46 -0700

    Scaffold commit.

--- samza-test/src/main/python/tests/standalone_tests.py
+++ samza-test/src/main/python/tests/standalone_tests.py
@@ -160,4 +160,4 @@ def _load_data():
            producer.send_messages(TEST_INPUT_TOPIC, str(i))
        kafka.close()
     except:
-       logger.error("Unexpected error: {0}".format(sys.exc_info()[0]))
+       logger.error("Unexpected error: {0}, {1}, {2}".format(sys.exc_info()[0], sys.exc_info()[1], sys.exc_info()[2]))
