31dd1dc6af1680fcb6aab795001a3c1d69e9a566
Shanthoosh Venkataraman <santhoshvenkat1988@gmail.com>
2018-06-08 18:31:42 -0700

    Scaffold commit.

--- samza-test/src/main/python/tests/standalone_tests.py
+++ samza-test/src/main/python/tests/standalone_tests.py
@@ -34,110 +34,118 @@ from zopkio.deployer import Deployer, Process
 from zopkio.remote_host_helper import better_exec_command, DeploymentError, get_sftp_client, get_ssh_client, open_remote_file, log_output, exec_with_env
 from standalone_processor import StandaloneProcessor
 import zk_util
+import os
+import shutil
+import unittest
 
-logger = logging.getLogger(__name__)
+from zopkio.deployer import Deployer, Process
+from zopkio.remote_host_helper import ParamikoError, better_exec_command, get_ssh_client, copy_dir, get_sftp_client
+from .mock import Mock_Deployer
 
-APP_NAME = 'test-app-name'
-APP_ID = 'test-app-id'
-ZK_BASE_DIR='app-{0}-{1}/{2}-{3}-coordinationData'.format(APP_NAME, APP_ID, APP_NAME, APP_ID)
 
-PACKAGE_ID = 'tests'
-TEST_INPUT_TOPIC = 'standaloneIntegrationTestKafkaInputTopic'
-TEST_OUTPUT_TOPIC = 'standaloneIntegrationTestKafkaOutputTopic'
-NUM_MESSAGES = 50
-JOB_MODEL_TIMEOUT = 6
-processors = {}
+class TestDeployer(unittest.TestCase):
 
-def _load_data():
-    kafka = None
-    try:
-        logger.info("load-data")
-        kafka = util.get_kafka_client()
-        kafka.ensure_topic_exists(TEST_INPUT_TOPIC)
-        producer = SimpleProducer(
-            kafka,
-            async=False,
-            req_acks=SimpleProducer.ACK_AFTER_CLUSTER_COMMIT,
-            ack_timeout=30000)
-        for i in range(1, NUM_MESSAGES + 1):
-            logger.info('Publishing message to topic: {0}'.format(TEST_INPUT_TOPIC))
-            producer.send_messages(TEST_INPUT_TOPIC, str(i))
-    except:
-        logger.error(traceback.format_exc(sys.exc_info()))
-    finally:
-        if kafka is not None:
-            kafka.close()
-
-def setup():
-    global processors
-    _load_data()
+    logger = logging.getLogger(__name__)
+    
+    APP_NAME = 'test-app-name'
+    APP_ID = 'test-app-id'
+    ZK_BASE_DIR='app-{0}-{1}/{2}-{3}-coordinationData'.format(APP_NAME, APP_ID, APP_NAME, APP_ID)
+    
+    PACKAGE_ID = 'tests'
+    TEST_INPUT_TOPIC = 'standaloneIntegrationTestKafkaInputTopic'
+    TEST_OUTPUT_TOPIC = 'standaloneIntegrationTestKafkaOutputTopic'
+    NUM_MESSAGES = 50
+    JOB_MODEL_TIMEOUT = 6
     processors = {}
-    for processor_id in ['standalone-processor-1', 'standalone-processor-2', 'standalone-processor-3']:
-        processors[processor_id] = StandaloneProcessor(processor_id=processor_id, package_id=PACKAGE_ID, configs={})
-        processors[processor_id].deploy()
+    
+    def _load_data(self):
+        kafka = None
+        try:
+            logger.info("load-data")
+            kafka = util.get_kafka_client()
+            kafka.ensure_topic_exists(TEST_INPUT_TOPIC)
+            producer = SimpleProducer(kafka, async=False, req_acks=SimpleProducer.ACK_AFTER_CLUSTER_COMMIT, ack_timeout=30000)
+            for message_index in range(1, NUM_MESSAGES + 1):
+                logger.info('Publishing message to topic: {0}'.format(TEST_INPUT_TOPIC))
+                producer.send_messages(TEST_INPUT_TOPIC, str(message_index))
+        except:
+            logger.error(traceback.format_exc(sys.exc_info()))
+        finally:
+            if kafka is not None:
+                kafka.close()
+    
+    def setup(self):
+        global processors
+        _load_data()
+        processors = {}
+        for processor_id in ['standalone-processor-1', 'standalone-processor-2', 'standalone-processor-3']:
+            processors[processor_id] = StandaloneProcessor(processor_id=processor_id, package_id=PACKAGE_ID, configs={})
+            processors[processor_id].deploy()
+    
+    def teardown(self):
+        global processors
+    
+        for processor_id, processor in processors.iteritems():
+            logger.info("Killing processor: {0}.".format(processor_id))
+            processor.kill()
+    
+    def test_kill_master(self):
+        try:
+            leader_processor_id = zk_util.get_leader_processor_id(zk_base_dir=ZK_BASE_DIR)
+            processors.pop(leader_processor_id).kill()
+    
+            time.sleep(JOB_MODEL_TIMEOUT)
 
-def teardown():
-    global processors
+            job_model = zk_util.get_latest_job_model(zk_base_dir=ZK_BASE_DIR)
 
-    for processor_id, processor in processors.iteritems():
-        logger.info("Killing processor: {0}.".format(processor_id))
-        processor.kill()
-
-def test_kill_master():
-    try:
-        leader_processor_id = zk_util.get_leader_processor_id(zk_base_dir=ZK_BASE_DIR)
-        processors.pop(leader_processor_id).kill()
-
-        time.sleep(JOB_MODEL_TIMEOUT)
-
-        job_model = zk_util.get_latest_job_model(zk_base_dir=ZK_BASE_DIR)
-
-        for processor_id, deployer in processors.iteritems():
-            assert processor_id in job_model['containers'], 'Processor id: {0} does not exist in JobModel.'.format(processor_id)
-    finally:
-        logger.error(traceback.format_exc(sys.exc_info()))
-
-def test_kill_single_worker():
-    try:
-        leader_processor_id = zk_util.get_leader_processor_id(zk_base_dir=ZK_BASE_DIR)
-        for processor_id, deployer in processors.iteritems():
-            if processor_id != leader_processor_id:
+            for processor_id, deployer in processors.iteritems():
+                assert processor_id in job_model['containers'], 'Processor id: {0} does not exist in JobModel.'.format(processor_id)
+        finally:
+            logger.error(traceback.format_exc(sys.exc_info()))
+    
+    def test_kill_single_worker(self):
+        try:
+            leader_processor_id = zk_util.get_leader_processor_id(zk_base_dir=ZK_BASE_DIR)
+            for processor_id, deployer in processors.iteritems():
+                if processor_id != leader_processor_id:
+                    processors.pop(processor_id).kill()
+                    break
+    
+            time.sleep(JOB_MODEL_TIMEOUT)
+            job_model = zk_util.get_latest_job_model(zk_base_dir=ZK_BASE_DIR)
+            for processor_id, deployer in processors.iteritems():
+                assert processor_id in job_model['containers'], 'Processor id: {0} does not exist in JobModel.'.format(processor_id)
+        finally:
+            logger.error(traceback.format_exc(sys.exc_info()))
+    
+    def test_kill_multiple_workers(self):
+        try:
+            leader_processor_id = zk_util.get_leader_processor_id(zk_base_dir=ZK_BASE_DIR)
+            for processor_id in processors.keys():
+                if processor_id != leader_processor_id:
+                    follower = processors.pop(processor_id)
+                    follower.kill()
+    
+            time.sleep(JOB_MODEL_TIMEOUT)
+            job_model = zk_util.get_latest_job_model(zk_base_dir=ZK_BASE_DIR)
+            assert leader_processor_id in job_model['containers'], 'Processor id: {0} does not exist in JobModel.'.format(leader_processor_id)
+        finally:
+            logger.error(traceback.format_exc(sys.exc_info()))
+    
+    def test_kill_leader_and_follower(self):
+        try:  
+            leader_processor_id = zk_util.get_leader_processor_id(zk_base_dir=ZK_BASE_DIR)
+            processors.pop(leader_processor_id).kill()
+    
+            for processor_id in processors.keys():
                 processors.pop(processor_id).kill()
                 break
+    
+            time.sleep(JOB_MODEL_TIMEOUT)
+            job_model = zk_util.get_latest_job_model(zk_base_dir=ZK_BASE_DIR)
+            assert leader_processor_id in job_model['containers'], 'Processor id: {0} does not exist in JobModel.'.format(leader_processor_id)
+        finally:
+            logger.error(traceback.format_exc(sys.exc_info()))
 
-        time.sleep(JOB_MODEL_TIMEOUT)
-        job_model = zk_util.get_latest_job_model(zk_base_dir=ZK_BASE_DIR)
-        for processor_id, deployer in processors.iteritems():
-            assert processor_id in job_model['containers'], 'Processor id: {0} does not exist in JobModel.'.format(processor_id)
-    finally:
-        logger.error(traceback.format_exc(sys.exc_info()))
-
-def test_kill_multiple_workers():
-    try:
-        leader_processor_id = zk_util.get_leader_processor_id(zk_base_dir=ZK_BASE_DIR)
-        for processor_id in processors.keys():
-            if processor_id != leader_processor_id:
-                follower = processors.pop(processor_id)
-                follower.kill()
-
-        time.sleep(JOB_MODEL_TIMEOUT)
-        job_model = zk_util.get_latest_job_model(zk_base_dir=ZK_BASE_DIR)
-        assert leader_processor_id in job_model['containers'], 'Processor id: {0} does not exist in JobModel.'.format(leader_processor_id)
-    finally:
-        logger.error(traceback.format_exc(sys.exc_info()))
-
-def test_kill_leader_and_follower():
-    try:
-
-        leader_processor_id = zk_util.get_leader_processor_id(zk_base_dir=ZK_BASE_DIR)
-        processors.pop(leader_processor_id).kill()
-
-        for processor_id in processors.keys():
-            processors.pop(processor_id).kill()
-            break
-
-        time.sleep(JOB_MODEL_TIMEOUT)
-        job_model = zk_util.get_latest_job_model(zk_base_dir=ZK_BASE_DIR)
-        assert leader_processor_id in job_model['containers'], 'Processor id: {0} does not exist in JobModel.'.format(leader_processor_id)
-    finally:
-        logger.error(traceback.format_exc(sys.exc_info()))
+if __name__ == '__main__':
+  unittest.main()
--- samza-test/src/main/python/tests/zk_util.py
+++ samza-test/src/main/python/tests/zk_util.py
@@ -70,14 +70,11 @@ def get_latest_job_model(zk_base_dir):
     try:
         zk_client = KazooClient(hosts='127.0.0.1:2181')
         zk_client.start()
-
         childZkNodes = zk_client.get_children('{0}/JobModelGeneration/jobModels/'.format(zk_base_dir))
-        logger.info("Retrieved childNodes: {0}.".format(childZkNodes))
         childZkNodes.sort()
         childZkNodes.reverse()
 
         job_model_generation_path = '{0}/JobModelGeneration/jobModels/{1}/'.format(zk_base_dir, childZkNodes[0])
-        logger.info('Fetching jobModel from path: {0}.'.format(job_model_generation_path))
         job_model, _ = zk_client.get(job_model_generation_path)
 
         """ 
@@ -90,10 +87,7 @@ def get_latest_job_model(zk_base_dir):
 
         first_curly_index = job_model.find('{')
         job_model = job_model[first_curly_index: ]
-
         job_model_dict = json.loads(job_model)
-        logger.info('Retrieved job model.')
-        logger.info(job_model_dict)
         return job_model_dict
     finally:
         if zk_client is not None:
@@ -128,22 +122,16 @@ def get_all_processors(zk_base_dir):
         zk_client.start()
         processors_path =  '{0}/processors'.format(zk_base_dir)
 
-        logger.info("Invoking getChildren on path: {0}".format(processors_path))
-
         childZkNodes = zk_client.get_children(processors_path)
         childZkNodes.sort()
 
-        logger.info("ChildZNodes of parent path: {0} is {1}.".format(processors_path, childZkNodes))
         processor_ids = []
         for childZkNode in childZkNodes:
             child_processor_path = '{0}/{1}'.format(processors_path, childZkNode)
-            logger.info("Invoking getData on path: {0}".format(child_processor_path))
             processor_data, _ = zk_client.get(child_processor_path)
             host, processor_id = processor_data.split(" ")
             processor_ids.append(processor_id)
 
-        logger.info('Retrieved processors.')
-        logger.info(processor_ids)
         if len(processor_ids) > 0:
             return processor_ids[0]
         else:
@@ -158,17 +146,13 @@ def get_all_processors(zk_base_dir):
 def get_leader_processor_id(zk_base_dir):
     zk_client = None
     try:
-        logger.info("Executing get_leader_processor_id: {0}.".format(zk_base_dir))
         zk_client = KazooClient(hosts='127.0.0.1:2181')
         zk_client.start()
         processors_path =  '{0}/processors'.format(zk_base_dir)
 
-        logger.info("Invoking getChildren on path: {0}".format(processors_path))
-
         childZkNodes = zk_client.get_children(processors_path)
         childZkNodes.sort()
         child_processor_path = '{0}/{1}'.format(processors_path, childZkNodes[0])
-        logger.info("Invoking getData on path: {0}".format(child_processor_path))
         processor_data, _ = zk_client.get(child_processor_path)
         host, processor_id = processor_data.split(" ")
         return processor_id
